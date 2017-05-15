04df4d631307e4b4d4e53e9aaff543ca63c3c284
BadgerOps <github@badgerops.net>
2017-05-15 16:04:58 -0600

    whoops, that was used

--- libs/LcdPlate.py
+++ libs/LcdPlate.py
@@ -15,6 +15,7 @@ class LcdPlate(threading.Thread):
         self.sc = SpeedTestCheck
         threading.Thread.__init__(self)
         self.lcd_buttons = None
+        self.sc.messages['ip_addr'] = 1.2.3.4
         self.setup()
         self.buttons = [ LCD.SELECT, LCD.LEFT, LCD.UP, LCD.DOWN, LCD.RIGHT ]
         self.func_matrix = {
@@ -83,6 +84,9 @@ class LcdPlate(threading.Thread):
                     time.sleep(0.1) # don't run away...
                     for b in self.buttons:
                         if self.lcd.is_pressed(b):
-                            self.func_matrix[b](self.button_matrix(b))
+                            if b != 0: # don't run this for 'select'
+                                self.func_matrix[b](self.button_matrix(b))
+                            elif b == 0:
+                                self.func_matrix[b]()
             except Exception as e:
                 print 'Exception in LCD Loop: {0}'.format(e, exc_info=1)
